0dc67782ca8e28b2b4feb87eb0e006565d85fdee
Rinwu1114 <Rinwu1114@gmail.com>
2025-06-01 01:45:02 -0500

    pivoted from board games to pokemon tcg cards due to board game api not working

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log(`hello world`)
+
+async function main() {
+    const games = await fetch("https://rpggeek.com/xmlapi2/")
+    const gamesData = await games.json()
+    console.log(gamesData)
+}
+
+main()
+
